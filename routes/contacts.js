67d0a579e9d93ef9f39fb87195c9225ac3d3f1ab
lsteel <lorinsteel@gmail.com>
2016-04-21 23:32:38 -0600

    Installed body-parser as dependancy. Added express post functionality to contacts api.

--- routes/contacts.js
+++ routes/contacts.js
@@ -11,4 +11,28 @@ contactsRouter.get('/', (req, res, next) => {
   res.json(contacts)
 })
 
+contactsRouter.post('/', (req, res, next) => {
+  if (!req.body.name) {
+    return next(new Error('You must include a `name` in the body.'))
+  }
+  if (typeof req.body.name !== 'string') {
+    return next(new Error('Then `name` property must be a string.'))
+  }
+
+  let newContactId = contacts.push({
+    Name: req.body.Name,
+    Title: req.body.Title,
+    Phone: req.body.Phone,
+    Street: req.body.Street,
+    City: req.body.City,
+    State: req.body.State,
+    Zipcode: req.body.Zipcode,
+    Email: req.body.Email,
+    Birthday: req.body.Birthday,
+    Picture: req.body.Picture
+  })
+
+  res.json(contacts[newContactId])
+})
+
 export default contactsRouter
